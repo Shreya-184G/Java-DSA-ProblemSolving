dbc33c46543bfefd9aa75de4a6fa5e240b7672c8
Shreya-184G <shreyagujar379@gmail.com>
2026-09-11 22:41:59 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+/*public class Loops{
+    public static void main(String[] args) {
+        for(int i = 11;i<=17;i++){
+            System.out.print(i+" ");
+        }
+    }
+}*/
+
+/* Sum of n natural numbers :
+public class Loops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n :");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0 ; i<=n; i++){
+            sum = sum + i;  
+        }
+        System.out.println("The Sum of "+n+" natural numbers is: " +sum);
+        
+    }
+}
+*/
+
+//Print the table of number input by the user
+/* 
+public class Loops{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter which number's table you want :");
+        int n = sc.nextInt();
+        System.out.println("The table of "+n+ " is:" );
+        for(int i = 1; i<11;i++){
+            System.out.println(i*n);
+        }
+    }
+}
+*/
+
+//
+
+
+
